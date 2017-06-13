857abae11a62a88ed4782af325153cf1572a9c21
sunwonsara <sunwonsara@naver.com>
2017-06-13 20:58:04 +0900

    #5 Event Handling 3

--- app/src/main/java/jungdain/kr/hs/emirim/dbex/MainActivity.java
+++ app/src/main/java/jungdain/kr/hs/emirim/dbex/MainActivity.java
@@ -1,6 +1,7 @@
 package jungdain.kr.hs.emirim.dbex;
 
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.support.v7.app.AppCompatActivity;
@@ -52,6 +53,19 @@ public class MainActivity extends AppCompatActivity {
                 sqlDb.execSQL(sql);
                 sqlDb.close();
                 Toast.makeText(MainActivity.this,"저장됨",Toast.LENGTH_LONG).show();
+            }
+        });
+        butSelect.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                sqlDb=myHelper.getReadableDatabase();
+                String sql="select*from idolTable";
+                Cursor cursor=sqlDb.rawQuery(sql,null);
+                String names="Idol 이름"+"\r\n"+"=================="+"\r\n";
+                String counts="Idol 인원수"+"\r\n"+"=================="+"\r\n";
+                while(cursor.moveToNext()){
+
+                }
             }
         });
     }
