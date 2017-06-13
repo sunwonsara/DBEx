26ee19d0ff796411911a7de12bced0d53182955b
sunwonsara <sunwonsara@naver.com>
2017-06-13 20:33:21 +0900

    #3 Event Handling 1

--- app/src/main/java/jungdain/kr/hs/emirim/dbex/MainActivity.java
+++ app/src/main/java/jungdain/kr/hs/emirim/dbex/MainActivity.java
@@ -5,13 +5,44 @@ import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+    Button butInit;
+    Button butInsert;
+    Button butSelect;
+    EditText editName;
+    EditText editCount;
+    EditText editResultName;
+    EditText editResultCount;
+    MyDBHelper myHelper; //DB를 생성해줌
+    SQLiteDatabase sqlDb;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        editName=(EditText)findViewById(R.id.edit_group_name);
+        editCount=(EditText)findViewById(R.id.edit_group_count);
+        editResultName=(EditText)findViewById(R.id.edit_result_name);
+        editResultCount=(EditText)findViewById(R.id.edit_result_count);
+        butInit=(Button)findViewById(R.id.but_init);
+        butInsert=(Button)findViewById(R.id.but_insert);
+        butSelect=(Button)findViewById(R.id.but_select);
+
+        //DB생성
+        myHelper=new MyDBHelper(this);
+        //기존의 테이블이 존재하면 삭제하고 테이블을 새로 생성한다.
+        butInit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                sqlDb=myHelper.getWritableDatabase();
+                myHelper.onUpgrade(sqlDb,1,2);
+                sqlDb.close();
+            }
+        });
     }
 
     class MyDBHelper extends SQLiteOpenHelper {
